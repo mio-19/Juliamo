e5d6059145bb49b7ead7c6c7d3e718eee95bf0bb
澪 <mio@mio19.uk>
2023-01-31 07:19:16 +0800

    up

--- juliamo-eval/src/main/scala/juliamo/repr/ValueRepr.scala
+++ juliamo-eval/src/main/scala/juliamo/repr/ValueRepr.scala
@@ -2,4 +2,8 @@ package juliamo.repr
 
 sealed trait ValueRepr
 
-final case class RuntimeType(typ: TypeRepr)
+final case class RuntimeType(typ: TypeRepr) extends ValueRepr
+
+final case class Closure(args: Vector[String], body: ClosureBody) extends ValueRepr with HasID {
+  
+}
