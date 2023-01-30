cf5e357afbb6c58e3876e4d96ffc33da753909fe
澪 <mio@mio19.uk>
2023-01-31 07:14:54 +0800

    up

--- juliamo-eval/src/main/scala/juliamo/repr/id.scala
+++ juliamo-eval/src/main/scala/juliamo/repr/id.scala
@@ -6,6 +6,7 @@ final case class RefID(mostSigBits: Long, leastSigBits: Long)
 
 object RefID {
   def constant(x: String): RefID = throw new UnsupportedOperationException("TODO")
+
   val OnRec: RefID = RefID(0, 0)
 }
 
@@ -14,11 +15,19 @@ private val RecIDCatcher = new Parameter[Set[HasID]]
 trait HasID {
   protected def idImpl: RefID = throw new UnsupportedOperationException("TODO")
 
-  def id: RefID = RecIDCatcher.get match {
+  private var idCache: RefID = null
+
+  private def calculateId: RefID = RecIDCatcher.get match {
     case Some(history) if history.contains(this) => RefID.OnRec
     case _ => RecIDCatcher.callWithOrUpdate(Set(this), _.incl(this)) {
       idImpl
     }
+  }
+
+  def id: RefID = if (idCache != null) idCache else {
+    val result = calculateId
+    idCache = result
+    result
   }
 }
 
