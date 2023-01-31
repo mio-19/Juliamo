93350d2eda5a7487bef79c85311589ef2391c31c
澪 <mio@mio19.uk>
2023-01-31 15:48:04 +0800

    up

--- juliamo-eval/src/main/scala/juliamo/ast/Stmt.scala
+++ juliamo-eval/src/main/scala/juliamo/ast/Stmt.scala
@@ -1,11 +1,14 @@
 package juliamo.ast
 
-sealed trait Stmt extends HasPos
 
-final case class Return(override val pos: SourcePos, expr: Expr) extends Stmt
+sealed trait Stmt extends HasPos
 
 final case class Let(override val pos: SourcePos, id: String, expr: Expr) extends Stmt
 
-sealed trait Expr extends Stmt
-
-final case class Block(override val pos: SourcePos, stmts: Vector[Stmt], lastYield: Option[Expr]) extends Expr
+enum Expr extends Stmt:
+  case Return(override val pos: SourcePos, expr: Expr) extends Expr
+  case Resume(override val pos: SourcePos, expr: Expr) extends Expr
+  case Block(override val pos: SourcePos, body: Vector[Stmt]) extends Expr
+  case If(override val pos: SourcePos, condition: Expr, whenTrue: Option[Block], whenFalse: Option[Block])
+  case Ref(override val pos: SourcePos, qualify: Vector[String], name: String) extends Expr
+  case Call(override val pos: SourcePos, f: Expr, args: Vector[Expr])
