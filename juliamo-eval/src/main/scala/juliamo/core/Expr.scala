f0378c815bf184be369fad4d9f6efa8419f1fcac
澪 <mio@mio19.uk>
2023-02-01 04:17:06 +0800

    up

--- juliamo-eval/src/main/scala/juliamo/core/Expr.scala
+++ juliamo-eval/src/main/scala/juliamo/core/Expr.scala
@@ -5,11 +5,32 @@ import juliamo.hashrepr.ReprID
 
 type CoreID = ReprID
 
-enum CoreExpr extends HasPos:
-  case Lit(override val pos: SourcePos, x: Literal) extends CoreExpr
-  case PureFunctionCall(override val pos: SourcePos) extends CoreExpr // TODO
+sealed trait CoreExpr
+object CoreExpr {
+  final case class Lit(x: Literal) extends CoreExpr
+  final case class PureFunctionCall() extends CoreExpr // TODO
+}
 
+sealed trait CoreType extends CoreExpr {
+  def parents: Set[CoreType] = throw new UnsupportedOperationException("TODO")
+}
 
+object CoreType {
+  enum LitType extends CoreType:
+    case Integer extends LitType
+    case String extends LitType
+    case Character extends LitType
+
+  case object Effect extends CoreType
+
+  final case class RecordType() extends CoreType // TODO
+
+  final case class UnionType(xs: Set[CoreExpr]) extends CoreType // TODO
+}
+
+final case class CoreEffect() extends CoreExpr // TODO
+
+final case class UnionEffect(xs: Set[CoreExpr]) extends CoreExpr
 
 enum CoreStmt extends HasPos:
   case Return(override val pos: SourcePos, expr: CoreExpr) extends CoreStmt
