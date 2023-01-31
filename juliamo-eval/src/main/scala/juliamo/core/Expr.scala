fe58d6bbd094753aaeeca411d3f77f6d1de1e3d6
澪 <mio@mio19.uk>
2023-02-01 04:29:37 +0800

    up

--- juliamo-eval/src/main/scala/juliamo/core/Expr.scala
+++ juliamo-eval/src/main/scala/juliamo/core/Expr.scala
@@ -3,9 +3,45 @@ package juliamo.core
 import juliamo.ast.{HasPos, Literal, SourcePos}
 import juliamo.hashrepr.ReprID
 
+import scala.ref.WeakReference
+
 type CoreID = ReprID
 
-sealed trait CoreExpr
+final case class Subst() // TODO
+
+sealed trait CoreExpr {
+  def subst(subst: Subst): CoreExpr = ???
+
+  private var whnfCache = new WeakReference[CoreExpr](null)
+
+  private var normalizeCache = new WeakReference[CoreExpr](null)
+
+  final def whnf: CoreExpr = whnfCache.get match {
+    case Some(x) if x != null => x
+    case _ => {
+      val x = whnfImpl
+      val whnfCache = new WeakReference(x)
+      this.whnfCache = whnfCache
+      x.whnfCache = whnfCache
+      x
+    }
+  }
+
+  def whnfImpl: CoreExpr = ??? // TODO
+
+  final def normalize: CoreExpr = normalizeCache.get match {
+    case Some(x) if x != null => x
+    case _ => {
+      val x = normalizeImpl
+      val normalizeCache = new WeakReference(x)
+      this.normalizeCache = normalizeCache
+      x.normalizeCache = normalizeCache
+      x
+    }
+  }
+
+  def normalizeImpl: CoreExpr = ??? // TODO
+}
 object CoreExpr {
   final case class Lit(x: Literal) extends CoreExpr
   final case class PureFunctionCall() extends CoreExpr // TODO
@@ -36,6 +72,6 @@ enum CoreStmt extends HasPos:
   case Return(override val pos: SourcePos, expr: CoreExpr) extends CoreStmt
   case Resume(override val pos: SourcePos, expr: CoreExpr) extends CoreStmt
   case FunctionCallBind(override val pos: SourcePos) extends CoreStmt // TODO
-  
+
 
 final case class CoreError() extends CoreExpr // TODO // used in typechecking
