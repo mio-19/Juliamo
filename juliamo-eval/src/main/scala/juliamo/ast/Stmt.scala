3ba53f4a03b8fe89ff55c385de8b12203e233d90
澪 <mio@mio19.uk>
2023-01-31 20:52:22 +0800

    up

--- juliamo-eval/src/main/scala/juliamo/ast/Stmt.scala
+++ juliamo-eval/src/main/scala/juliamo/ast/Stmt.scala
@@ -10,16 +10,20 @@ final case class QualifiedName(qualify: Vector[String], name: String)
 
 final case class Arg(name: String, typeExpr: Expr)
 
-enum Expr extends Stmt:
-  case Return(override val pos: SourcePos, expr: Expr) extends Expr
-  case Resume(override val pos: SourcePos, expr: Expr) extends Expr
-  case Block(override val pos: SourcePos, body: Vector[Stmt]) extends Expr
-  case If(override val pos: SourcePos, condition: Expr, whenTrue: Option[Block], whenFalse: Option[Block]) extends Expr
-  case Ref(override val pos: SourcePos, name: QualifiedName) extends Expr
-  case Call(override val pos: SourcePos, f: Expr, args: Vector[Expr]) extends Expr
-  case Lambda(override val pos: SourcePos, args: Vector[Arg], body: Expr.Block) extends Expr
-  case Lit(override val pos: SourcePos, x: Literal) extends Expr
+sealed trait Expr extends Stmt
+object Expr {
+  final case class Return(override val pos: SourcePos, expr: Expr) extends Expr
+  final case class Resume(override val pos: SourcePos, expr: Expr) extends Expr
+  final case class Block(override val pos: SourcePos, body: Vector[Stmt]) extends Expr
+  final case class If(override val pos: SourcePos, condition: Expr, whenTrue: Option[Block], whenFalse: Option[Block]) extends Expr
+  final case class Ref(override val pos: SourcePos, name: QualifiedName) extends Expr
+  final case class Call(override val pos: SourcePos, f: Expr, args: Vector[Expr]) extends Expr
+  final case class Lambda(override val pos: SourcePos, args: Vector[Arg], body: Expr.Block) extends Expr
+  final case class Lit(override val pos: SourcePos, x: Literal) extends Expr
+}
 
+
+sealed trait Universe extends Expr
 
 enum Literal:
   case Integer(x: prim.Integer) extends Literal
--- juliamo-eval/src/main/scala/juliamo/core/Expr.scala
+++ juliamo-eval/src/main/scala/juliamo/core/Expr.scala
@@ -4,3 +4,9 @@ import juliamo.ast.{HasPos, Literal, SourcePos}
 
 enum CoreExpr extends HasPos:
   case Lit(override val pos: SourcePos, x: Literal) extends CoreExpr
+
+
+
+enum CoreStmt extends HasPos:
+  case Return(override val pos: SourcePos, expr: CoreExpr) extends CoreStmt
+  case Resume(override val pos: SourcePos, expr: CoreExpr) extends CoreStmt
